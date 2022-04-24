5961fd261acff2b50aadaae582b7f61756608747
nick <nick@nicklashbrook.com>
2022-04-24 16:01:48 -0500

    Update User model to authenticate users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,8 +50,14 @@ end
 
 test "email addresses should be unique" do
   duplicate_user = @user.dup
-  duplicate_user.email = @user.email.upcase
   @user.save
   assert_not duplicate_user.valid?
-  end 
+  end
+
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end
